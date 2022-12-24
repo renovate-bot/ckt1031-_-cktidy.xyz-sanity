e7765a23f4b744abe7159a2f94c05e3a4bf60e1d
cktsun1031 <cktsun1031@gmail.com>
2022-12-24 11:30:49 +0800

    feat: bump deps

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,7 +4,7 @@ import { visionTool } from '@sanity/vision';
 import { schemaTypes } from './schemas';
 import { markdownSchema } from 'sanity-plugin-markdown';
 import { codeInput } from '@sanity/code-input';
-import {media} from 'sanity-plugin-media'
+import { media } from 'sanity-plugin-media';
 
 export default defineConfig({
   name: 'cktidy',
